78930bf36dfd9143da41a242c0207bd1cd940bfb
Lee Williams | Work <alphauk101@hotmail.com>
2015-05-01 10:25:09 +0100

    Commit of initial dev

---
+++ BluGiga-BDS-Plugin/Plugin.js
@@ -0,0 +1,18 @@
+﻿function GetInfo(infoObject) {
+    infoObject.Name = "Bluegiga XML Profile (Alpha)";
+    infoObject.Description = "To convert a profile Bluegiga XML File";
+    infoObject.Author = "Lee Williams (github: alphauk101";
+    infoObject.Version = "0.1";
+    infoObject.IsClient = true;
+    return infoObject;
+}
+
+function RunPlugin(profileData) {
+	//Get the template file for standard XML Bluegiga project.
+	var templateFileContents = FileManager.ReadFile("blgig_V0.tlp");
+	outputTemplate = ProcessTemplate(templateFileContents, profileData);
+    FileManager.CreateFile("gatt113.xml", outputTemplate);
+}
+
+
+
